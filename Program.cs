7149ccf4c06d04f3107afa6510382feb6e63127b
ryanzinho666 <126687250+ben-gadura@users.noreply.github.com>
2023-03-22 12:28:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ativiade_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int laranja;
+            //float t;
+            //Console.WriteLine("quantas laranjas foram compradas?");
+            //laranja = int.Parse(Console.ReadLine());
+            //if (laranja < 12) 
+            //t = laranja * 0.35f;
+            //else
+            //    t = laranja * 0.28f;
+            //Console.WriteLine("total é" + t);
+            //Console.ReadKey();
+
+
+
+//int  lados;
+//Console.WriteLine("quantos lados no triangulo possuem medidas iguais?");
+//lados = int.Parse(Console.ReadLine());
+//if (lados == 3)
+//{ Console.WriteLine("é um triangulo equilátero"); }
+//if (lados == 2)
+//{ Console.WriteLine("é um triangulo isósceles"); }
+//if (lados == 0)
+//{ Console.WriteLine("é um triangulo escaleno"); }
+//Console.ReadKey();
+
+
+
+//int a, an, i, im;
+//im = 16;
+//Console.WriteLine("Digite o ano em que você nasceu. ");
+//an = int.Parse(Console.ReadLine());
+//Console.WriteLine("Digite o ano em que você está. ");
+//a = int.Parse(Console.ReadLine());
+//i = a - an;
+//if (i < im)
+//    Console.WriteLine("Você não poderá votar este ano.");
+//else if (i >= im)
+//    Console.WriteLine("Você poderá votar este ano.");
+//Console.ReadKey();
+
+
+//double a, b, c, d, x1, x2;
+//Console.WriteLine("Digite o valor de a, b, e c em ordem.");
+//a = double.Parse(Console.ReadLine());
+//b = double.Parse(Console.ReadLine());
+//c = double.Parse(Console.ReadLine());
+//d = (b * b) - 4 * a * c;
+//r1 = (-b + (Math.Sqrt(d))) / (2 * a);
+//r2 = (-b - (Math.Sqrt(d))) / (2 * a);
+//Console.WriteLine("A raiz X1 é igual a" + x1);
+//Console.WriteLine("A raiz X2 é igual a" + x2);
+//Console.ReadKey();
+
+
+//int valor;
+//Console.WriteLine("digite um valor");
+//valor = int.Parse(Console.ReadLine());
+//if ((valor % 5) == 0)
+//    Console.WriteLine("Este número é divisível por 5. ");
+//else if ((valor % 3) == 0) 
+//    Console.WriteLine("Este número é divisível por 3. ");
+//else
+//    Console.WriteLine("Este número não é divisível nem por 3, nem por 5. ");
+//Console.ReadKey();
+
+
+//double a1, a2, a3;
+//Console.WriteLine("digite os 3 ângulos do seu triângulo. ");
+//a1 = double.Parse(Console.ReadLine());
+//a2 = double.Parse(Console.ReadLine());
+//a3 = double.Parse(Console.ReadLine());
+//if ((a1 == 90) || (a2 == 90) || (a3 == 90))
+//    Console.WriteLine("Seu triângulo é um triângulo retângulo. ");
+//else if ((a1 > 90) || (a2 > 90) || (a3 > 90))
+//    Console.WriteLine("Seu triângulo é um triângulo obtusângulo. ");
+//else if ((a1 > 90) && (a2 > 90) && (a3 > 90))
+//    Console.WriteLine("Seu triângulo é um triângulo acutângulo. ");
+//Console.ReadKey();
+
+
+//int n1, n2, n3;
+//Console.WriteLine("digite 3 números. ");
+//n1 = int.Parse(Console.ReadLine());
+//n2 = int.Parse(Console.ReadLine());
+//n3 = int.Parse(Console.ReadLine());
+//if ((n1 < n2 && n2 < n3))
+//    Console.WriteLine(n1 + " " + n2 + " " + n3);
+//else if (n1 < n2 && n2 > n3 && n3 > n1)
+//    Console.WriteLine(n1 + " " + n3 + " " + n2);
+//else if (n2 < n1 && n1 < n3)
+//    Console.WriteLine(n2 + " " + n1 + " " + n3);
+//else if (n2 < n1 && n3 < n1 && n2 < n3)
+//    Console.WriteLine(n2 + " " + n3 + " " + n1);
+//else if (n3 < n1 && n1 < n2)
+//    Console.WriteLine(n3 + " " + n1 + " " + n2);
+//else if (n3 < n2 && n2 < n1 && n3 < n2)
+//    Console.WriteLine(n3 + " " + n2 + " " + n1);
+//Console.ReadKey();
+
+
+//int ano;
+//Console.WriteLine("Digite um ano. ");
+//ano = int.Parse(Console.ReadLine());
+//if ((ano % 4) == 0)
+//    Console.WriteLine("Seu ano é bissexto. ");
+//else
+//    Console.WriteLine("Seu ano não é bissexto. ");
+//Console.ReadKey();
+
+
+//double n1, n2, result;
+//string operacao;
+//Console.WriteLine("digite um valor. ");
+//n1 = double.Parse(Console.ReadLine());
+//Console.WriteLine("digite um segundo valor. ");
+//n2 = double.Parse(Console.ReadLine());
+//Console.WriteLine("digite a operação que você deseja. ");
+//operacao = (Console.ReadLine());
+//result = 0;
+//if (operacao == "+")
+//    result = n1 + n2;
+//else if (operacao == "-")   
+//    result = n1 - n2;
+//else if (operacao == "*")  
+//    result = n1 * n2;
+//else if (operacao == "/")
+//    result = n1 / n2;
+//Console.WriteLine("o resultado da operação é " + result);
+//Console.ReadKey();
+
+
+/* exercicio 10
+ Gerar números aleatórios (ou randômicos) na linguagem C# é simples: basta usar a classe Random.
+Random randNum = new Random();
+randNum.Next();
+/*
+
+}
+}
+}
